c5855f3fd67528658816da4c2aa59e41cbb63720
Ayoub ASSAOUD <86426829+Ayoub-Vip@users.noreply.github.com>
2021-10-28 21:11:19 +0200

    Create code.py

---
+++ code.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#  Q2
+"""
+Created on Thu Oct 21 23:16:33 2021
+
+@author: 
+
+  Q1
+"""
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.integrate import solve_ivp as si
+
+def syst(t, y, sigma, r, beta):
+    dx = sigma*(y[1] - y[0])
+    dy = y[0] * (r - y[2]) - y[1]
+    dz = y[0] * y[1] - beta * y[2]
+    return (dx, dy, dz)
+
+C0 = [1, 1, 1]
+temps = [0, 100]
+sigma = 10
+beta = 8/3
+r = 10
+
+sol = si(lambda t, y: syst(t, y, sigma, r, beta), temps, C0, rtol=10**-9, atol=10**-9)
+
+plt.figure()
+plt.plot(sol.y[0,:],sol.y[1,:], color='#A23BEC');
+plt.xlabel('$X$')
+plt.ylabel('$Y$')
+
+plt.figure()
+plt.plot(sol.y[0,:],sol.y[2,:], color='#A23BEC');
+plt.xlabel('$X$')
+plt.ylabel('$Z$')
+
+
+
+plt.figure()
+plt.plot(sol.y[1,:],sol.y[2,:], color='#A23BEC');
+plt.xlabel('$Y$')
+plt.ylabel('$Z$')
